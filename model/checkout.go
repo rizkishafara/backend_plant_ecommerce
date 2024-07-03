ee10aade57e39de5440820719709994d645505ff
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2024-07-03 21:41:26 +0700

    refactor: Update GetShipping and GetPayment routes to format and include additional information

--- model/checkout.go
+++ model/checkout.go
@@ -1,8 +1,23 @@
 package model
 
-func Checkout(user_id,json_alamat,array_cartid,idshipping,idvoucher,idpayment string) (map[string]interface{}, error) {
+import (
+	"tanaman/db"
+	"tanaman/utils"
+)
 
-	
+func Checkout(user_id, order_number, payment_id, json_alamat, notes, resi, sub_total, discount, idshipping, date_create string) utils.Respon {
 
-	return nil, nil
+	dbEngine := db.ConnectDB()
+	var Respon utils.Respon
+
+	_, err := dbEngine.QueryString(``)
+	if err != nil {
+		Respon.Status = 500
+		Respon.Message = err.Error()
+		return Respon
+	}
+	Respon.Status = 200
+	Respon.Message = "success"
+
+	return Respon
 }
